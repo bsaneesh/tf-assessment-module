290d1c8c171d13dad9d5af664eb974b1d2c44b6d
Aneesh B S <Aneesh.B.S@kyndryl.com>
2023-09-14 02:25:07 +0530

    updated rg tf file

--- azrg.tf
+++ azrg.tf
@@ -1,5 +1,5 @@
-# resource "azurerm_resource_group" "tfassmnt-rg" {
-#     name = var.resource_group
-#     location = var.location
-#     tags = local.tag
-# }
+resource "azurerm_resource_group" "tfassmnt-rg" {
+    name = var.resource_group
+    location = var.location
+    tags = local.tag
+}
